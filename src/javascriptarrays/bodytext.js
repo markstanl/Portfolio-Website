68797261847ddcfd03eb963280d4e368e96a2285
Mark Stanley <146277800+markstanl@users.noreply.github.com>
2023-12-28 13:27:42 -0600

    Delete src directory

--- src/javascriptarrays/bodytext.js
+++
@@ -1,7 +0,0 @@
-export const bodytext = [
-    {title:'Synopsis', l1: 'UW-Madison Undergraduate Student', l2: 'Computer Science and Mathematics Dual Degree'},
-    {title:'Skills', l1: 'Java, Python, HTML, CSS, reactjs', l2: 'Drive to challenge myself'},
-    {title:'Projects', l1: 'Java Determinant and Runtime Calculator', l2: 'React Spotify API Integrated Website'},
-
-
-]
