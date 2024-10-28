2bcec88f5fcab322de43a03bbf58745ba54901f2
Mark Stanley <markgstanley1@gmail.com>
2024-10-28 15:50:34 -0500

    Update bodytext.js

--- src/javascriptarrays/bodytext.js
+++ src/javascriptarrays/bodytext.js
@@ -3,12 +3,12 @@ export const bodytext = [
     title: "Synopsis",
     l1: "UW-Madison Undergraduate Student",
     l2: "Computer Science and Mathematics Double Major",
-    l3: "Working as a free lance web developer. Looking for enthusiastic clients in need of a website.",
+    l3: "Working as a Software Technical Lead at the Wisconsin Messenger.",
   },
   {
     title: "Skills",
     l1: "Java, Python, React, Typescript, React",
-    l2: "Full Stack Development: Vite, Tailwind CSS, Node.js, Express.js, MongoDB, Github",
+    l2: "Full Stack Development: Next.js/Vite, React, Tailwind CSS, Express.js, MongoDB, Github",
     l3: "Data Structures and Algorithms, Object Oriented Programming, Web Development",
   },
   {
